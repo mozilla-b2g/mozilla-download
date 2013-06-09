beb2ef9b9e4676707fd909d635ec5a79a075e0e2
James Lal <james@lightsofapollo.com>
2013-06-09 00:01:50 -0700

    working linux downloads

--- test/download-test.js
+++ test/download-test.js
@@ -3,7 +3,7 @@ suite('download', function() {
   var runner = require('../lib/download');
   var fs = require('fs');
 
-  suite.only('mac64', function() {
+  suite('mac64', function() {
     var path = __dirname + '/darwin-out/';
     if (process.platform !== 'darwin')
       return test('cannot run mac64 tests on non darwin platforms');
@@ -17,7 +17,7 @@ suite('download', function() {
     });
   });
 
-  suite('linux-x86', function() {
+  suite.only('linux-x86', function() {
     var path = __dirname + '/linux-out/';
     if (process.platform === 'win32')
       return test('cannot run on windows');
